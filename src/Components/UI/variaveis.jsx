b77c291ab2b56f5458e58a1453e3ac0da38431f8
JosafaSilveira <josafajosina@gmail.com>
2023-05-17 18:24:47 -0300

    smart bank - first commit

---
+++ src/Components/UI/variaveis.jsx
@@ -0,0 +1,10 @@
+/* Claro */
+export const corPrimaria = '#41d3be';
+export const conteudoClaro = '#ffffff'
+export const fundoClaro = 'f1f1f1';
+export const textoFundoClaro = 'grey'
+
+/* Escuro */
+export const fundoEscuro  = '#363537';
+export const conteudoEscuro = '#5c5b5e';
+export const textoFundoEscuro = '#fafafa'
